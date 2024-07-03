4524e38ca63ada047c94bfdd23d2e62377690b2a
Abhimanyu Kumar <58160340+Abhimanyukumar-lab@users.noreply.github.com>
2024-07-03 17:31:41 +0530

    add test
    to see ci/cd

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
-class Main{
+class Main {
   public static void main(String[] args) {
     System.out.println("This is jenkins ");
     System.out.println("Hello World");
     System.out.println("Hello Abhimanyu");
     System.out.println("Hello DevOps");
-    
+
     System.out.println("=============================I LOVE JAVA.=========");
 
+    System.out.println("ci/cd");
+    System.out.println("ci/cd");
     System.out.println("ci/cd");
 
     Student student = new Student();
